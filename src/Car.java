899e4c6fa01b71ef4d05995500b7405135607021
Mateusz Barzantny <mateusz.barzantny@op.pl>
2018-05-21 20:07:52 +0200

    naprawiony kod

---
+++ src/Car.java
@@ -0,0 +1,15 @@
+class Car {
+    String brand;
+    String model;
+    int year;
+    double capacity;
+
+    Car(String b, String m, int yr, double ca){
+        brand = b;
+        model = m;
+        year = yr;
+        capacity = ca;
+
+
+    }
+}
